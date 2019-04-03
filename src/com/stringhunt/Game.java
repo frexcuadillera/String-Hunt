90ddb55d922e7e6b0b3a23d7f67616a0a3c48199
frexcuadillera <fcuadillera@gmail.com>
2019-04-04 00:59:44 +0800

    Create Game.java

---
+++ src/com/stringhunt/Game.java
@@ -0,0 +1,159 @@
+package com.stringhunt;
+
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import com.stringhunt.display.Display;
+import com.stringhunt.gfx.Assets;
+import com.stringhunt.input.KeyManager;
+import com.stringhunt.states.GameState;
+import com.stringhunt.states.MenuState;
+import com.stringhunt.states.State;
+
+public class Game implements Runnable{
+
+    private Display display;
+    public int width, height;
+    public String title;
+    
+    private boolean running = false;
+    private Thread thread;
+    
+    private BufferStrategy bs;
+    private Graphics g;
+    
+    //States
+    private State gameState;
+    private State menuState;
+    
+    //input
+    private KeyManager keyManager;
+      
+    public Game(String title, int width, int height) {
+	this.width = width;
+	this.height = height;
+	this.title = title;
+	keyManager = new KeyManager();
+	
+    }
+    
+    //initialize here
+    private void init() {
+	display = new Display(title, width, height);
+	display.getFrame().addKeyListener(keyManager);
+	Assets.init();
+	
+	//initialize states
+	gameState = new GameState(this);
+	menuState = new MenuState(this);
+	
+	//set init state
+	State.setState(menuState);
+	
+    }
+    
+    private void tick() {
+	keyManager.tick();
+	
+	if(State.getState() != null) {
+	    State.getState().tick();
+	}
+	
+    }
+    
+    private void render() {
+	bs = display.getCanvas().getBufferStrategy();
+	
+	if(bs == null) {
+	    display.getCanvas().createBufferStrategy(3);
+	    return;
+	}
+	
+	g = bs.getDrawGraphics();
+	
+	//clear screen
+	
+	g.clearRect(0, 0, width, height);
+	
+	
+	//draw here--------------------------------
+	
+	//g.drawImage(Assets.spriteName, xpos, ypos, null);
+	//g.drawImage(Assets.img1,x,50 ,null);
+	
+	if(State.getState() != null) {
+	    State.getState().render(g);
+	}
+	
+	//end drawing------------------------------
+	
+	bs.show();
+	g.dispose();
+    }
+    
+    public void run() {
+	
+	init();
+	
+	int fps = 60;
+	double timePerTick = 100000000 / fps;
+	double delta = 0;
+	long now;
+	long lastTime = System.nanoTime();
+	long timer = 0;
+	int ticks = 0;
+	
+	while(running) {
+	    
+	    now = System.nanoTime();
+	    delta += (now - lastTime) / timePerTick;
+	    timer += now - lastTime;
+	    lastTime = now;
+	    
+	    if (delta >= 1) {
+		tick();
+		render();
+		ticks++;
+		delta--;
+	    }
+	    
+	    if(timer >= 100000000) {
+		//System.out.println("Ticks and Frames: " + ticks);
+		ticks = 0;
+		timer = 0;
+	    }
+	    
+	}
+	
+	stop();
+    }
+    
+    public KeyManager getKeyManager() {
+	return keyManager;
+    }
+    
+    
+    public synchronized void start() {
+	if(running) {
+	    return;
+	}	   
+	
+	running = true;
+	thread = new Thread(this);
+	thread.start();
+    }
+    
+    public synchronized void stop() {
+	if(!running)
+	    return;
+	
+	running = false;
+	
+	try {
+	    thread.join();
+	} catch (InterruptedException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
+}
