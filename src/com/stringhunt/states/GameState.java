3465078061ec9b3ab565d580c13420f56f3c9dae
frexcuadillera <fcuadillera@gmail.com>
2019-04-04 00:58:41 +0800

    Create GameState.java

---
+++ src/com/stringhunt/states/GameState.java
@@ -0,0 +1,37 @@
+package com.stringhunt.states;
+
+import java.awt.Graphics;
+
+import com.stringhunt.Game;
+import com.stringhunt.entities.creatures.Player;
+import com.stringhunt.gfx.Assets;
+
+public class GameState extends State {
+    
+    
+    //declare game entities here
+    private Player player;
+    
+    public GameState(Game game) {
+	super(game);
+	
+	//player = new Player(xpos,ypos, width,height);
+	player = new Player(game, 100, 100, 50, 50);
+	
+    }
+
+    @Override
+    public void tick() {
+	player.tick();
+	
+    }
+
+    @Override
+    public void render(Graphics g) {
+	player.render(g);
+	
+    }
+    
+    
+
+}
