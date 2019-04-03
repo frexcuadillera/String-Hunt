a7a4c482f99ad0d8408d6b0f00b1c51ef7a76241
frexcuadillera <fcuadillera@gmail.com>
2019-04-04 00:59:31 +0800

    Create Launcher.java

---
+++ src/com/stringhunt/Launcher.java
@@ -0,0 +1,10 @@
+package com.stringhunt;
+
+public class Launcher {
+    
+    public static void main(final String [] args) {
+	Game game = new Game("String Hunt", 800, 450);
+	game.start();
+    }
+
+}
