182047fefc25618cb434d079bcd17c483b887138
frexcuadillera <fcuadillera@gmail.com>
2019-04-06 21:55:32 +0800

    use handler

--- src/com/stringhunt/states/GameState.java
+++ src/com/stringhunt/states/GameState.java
@@ -2,7 +2,7 @@ package com.stringhunt.states;
 
 import java.awt.Graphics;
 
-import com.stringhunt.Game;
+import com.stringhunt.Handler;
 import com.stringhunt.entities.creatures.Player;
 import com.stringhunt.gfx.Assets;
 
@@ -12,11 +12,12 @@ public class GameState extends State {
     //declare game entities here
     private Player player;
     
-    public GameState(Game game) {
-	super(game);
+    public GameState(Handler handler) {
+	super(handler);
+	
 	
 	//player = new Player(xpos,ypos, width,height);
-	player = new Player(game, 100, 100, 50, 50);
+	player = new Player(handler, 100, 100, 50, 50);
 	
     }
 
