10668afbd0b718b2d6c63af25e78508c00e57dc8
frexcuadillera <fcuadillera@gmail.com>
2019-04-06 23:20:46 +0800

    added ui sheet to assets

--- src/com/stringhunt/gfx/Assets.java
+++ src/com/stringhunt/gfx/Assets.java
@@ -4,25 +4,23 @@ import java.awt.image.BufferedImage;
 
 public class Assets {
     
-    private static final int width = 50, height = 50;
-    
-    public static BufferedImage img1, img2;
+    public static BufferedImage[] btn_start;
     
     public static void init() {
 	//sprite sheet declaration
 	
 	//load image here
 	//testImage = ImageLoader.loadImage("/textures/test.jpg");
+	//btn_start[0] = ImageLoader.loadImage("/textures/blue_button00.jpg");
+	//btn_start[1] = ImageLoader.loadImage("/textures/blue_button01.jpg");
 	
-	//load spritesheet here
+	//load spritesheet heres
 	//load image first to convert as spritesheet
 	//sheet = new SpriteSheet(bufferedimagename);
-	//sheet = new SpriteSheet(testImage);
 	
-	SpriteSheet sheet  = new SpriteSheet(ImageLoader.loadImage("/textures/test.jpg"));
-	
-	img1 = sheet.crop(0, 0, width, height);
-	img2 = sheet.crop(50, 50, width, height);
+	SpriteSheet ui_sheet = new SpriteSheet(ImageLoader.loadImage("/textures/test.jpg"));	
+	btn_start[0] = ui_sheet.crop(0, 0, 190, 49);
+	btn_start[1] = ui_sheet.crop(0, 50, 190, 49);
     }
 
 }
