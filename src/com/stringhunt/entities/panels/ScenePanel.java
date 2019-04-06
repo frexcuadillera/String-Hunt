d1e65e2f300e6f340934957f463991c11eccb192
frexcuadillera <fcuadillera@gmail.com>
2019-04-06 21:55:51 +0800

    Create ScenePanel.java

---
+++ src/com/stringhunt/entities/panels/ScenePanel.java
@@ -0,0 +1,26 @@
+package com.stringhunt.entities.panels;
+
+import java.awt.Graphics;
+
+import com.stringhunt.entities.Entity;
+
+public class ScenePanel extends Entity {
+
+    public ScenePanel(float x, float y, int width, int height) {
+	super(x, y, width, height);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void tick() {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void render(Graphics g) {
+	// TODO Auto-generated method stub
+	
+    }
+
+}
