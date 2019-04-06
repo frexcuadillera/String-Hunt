f93582c3b86302fd5831789932aefc07d32fce9f
frexcuadillera <fcuadillera@gmail.com>
2019-04-07 01:53:04 +0800

    added main menu assets

--- src/com/stringhunt/states/MenuState.java
+++ src/com/stringhunt/states/MenuState.java
@@ -4,31 +4,66 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import com.stringhunt.Handler;
+import com.stringhunt.gfx.Assets;
+import com.stringhunt.ui.ClickListener;
+import com.stringhunt.ui.UIImageButton;
+import com.stringhunt.ui.UIManager;
 
 public class MenuState extends State {
     
+    private UIManager uiManager;
+    private final float X_POSITION = 300;
+    private final float Y_POSITION = 120;
+    private final float Y_OFFSET = 60;
+    
     public MenuState(Handler handler) {
 	super(handler);
+	uiManager = new UIManager(handler);
+	handler.getMouseManager().setUIManager(uiManager);
+		
+	uiManager.addObject(new UIImageButton(X_POSITION, Y_POSITION, 200, 50, Assets.btn_start, new ClickListener() {	    
+	    @Override
+	    public void onClick() {
+		State.setState(handler.getGame().gameState);
+	    }
+	    
+	}));
+	
+	uiManager.addObject(new UIImageButton(X_POSITION, Y_POSITION + Y_OFFSET, 200, 50, Assets.btn_tutorial, new ClickListener() {	    
+	    @Override
+	    public void onClick() {
+		State.setState(handler.getGame().tutorialState);
+	    }
+	    
+	}));
+	
+	uiManager.addObject(new UIImageButton(X_POSITION, Y_POSITION + (Y_OFFSET * 2), 200, 50, Assets.btn_credits, new ClickListener() {	    
+	    @Override
+	    public void onClick() {
+		State.setState(handler.getGame().creditsState);
+	    }
+	    
+	}));
+	
+	uiManager.addObject(new UIImageButton(X_POSITION, Y_POSITION + (Y_OFFSET * 3), 200, 50, Assets.btn_quit, new ClickListener() {	    
+	    @Override
+	    public void onClick() {
+		System.out.println("Quit button pressed");
+	    }
+	    
+	}));
 	
     }
 
     @Override
     public void tick() {
-	//System.out.println("x: " +handler.getMouseManager().getMouseX()+ " y: " +handler.getMouseManager().getMouseY());
-//	if(handler.getMouseManager().isLeftPressed()) {
-//	    State.setState(handler.getGame().gameState);
-//	}
-	
-	
-	
+	uiManager.tick();
     }
 
     @Override
     public void render(Graphics g) {
-	g.drawString("this is menu frame", 60, 60); //temp
-	g.setColor(Color.red);
-	g.fillRect(handler.getMouseManager().getMouseX(), handler.getMouseManager().getMouseY(), 8, 8);
-	
+	g.drawImage(Assets.background, 0, 0, null);
+	uiManager.render(g);
     }
 
 }
