550e9038adc6f40b9158b84fcc3bfb983d344149
frexcuadillera <fcuadillera@gmail.com>
2019-04-07 01:53:40 +0800

    added main menu assets

--- src/com/stringhunt/gfx/Assets.java
+++ src/com/stringhunt/gfx/Assets.java
@@ -4,23 +4,40 @@ import java.awt.image.BufferedImage;
 
 public class Assets {
     
+    public static BufferedImage background;
     public static BufferedImage[] btn_start;
+    public static BufferedImage[] btn_tutorial; 
+    public static BufferedImage[] btn_credits;
+    public static BufferedImage[] btn_quit;
     
     public static void init() {
-	//sprite sheet declaration
-	
+	//buffered declaration	
+	btn_start = new BufferedImage[2];
+	btn_tutorial = new BufferedImage[2];
+	btn_credits = new BufferedImage[2];
+	btn_quit = new BufferedImage[2];
 	//load image here
 	//testImage = ImageLoader.loadImage("/textures/test.jpg");
-	//btn_start[0] = ImageLoader.loadImage("/textures/blue_button00.jpg");
-	//btn_start[1] = ImageLoader.loadImage("/textures/blue_button01.jpg");
+	
+	
+	// main menu assets
+	background = ImageLoader.loadImage("/ui/background.jpg");
+	btn_start[0] = ImageLoader.loadImage("/ui/button_start_0.png");
+	btn_start[1] = ImageLoader.loadImage("/ui/button_start_1.png");
+	btn_tutorial[0] = ImageLoader.loadImage("/ui/button_tutorial_0.png");
+	btn_tutorial[1] = ImageLoader.loadImage("/ui/button_tutorial_1.png");	
+	btn_credits[0] = ImageLoader.loadImage("/ui/button_credits_0.png");
+	btn_credits[1] = ImageLoader.loadImage("/ui/button_credits_1.png");
+	btn_quit[0] = ImageLoader.loadImage("/ui/button_quit_0.png");
+	btn_quit[1] = ImageLoader.loadImage("/ui/button_quit_1.png");
 	
 	//load spritesheet heres
 	//load image first to convert as spritesheet
 	//sheet = new SpriteSheet(bufferedimagename);
 	
-	SpriteSheet ui_sheet = new SpriteSheet(ImageLoader.loadImage("/textures/test.jpg"));	
-	btn_start[0] = ui_sheet.crop(0, 0, 190, 49);
-	btn_start[1] = ui_sheet.crop(0, 50, 190, 49);
+	//SpriteSheet ui_sheet = new SpriteSheet(ImageLoader.loadImage("/sheets/blueSheet.png"));	
+	//btn_start[0] = ui_sheet.crop(0, 0, 190, 49);
+	//btn_start[1] = ui_sheet.crop(0, 50, 190, 49);
     }
 
 }
