7d4e737b6f859c2e9f4b195aa9316087f6742886
frexcuadillera <fcuadillera@gmail.com>
2019-04-06 21:55:02 +0800

    Create PausePanel.java

---
+++ src/com/stringhunt/entities/panels/PausePanel.java
@@ -0,0 +1,32 @@
+
+
+package com.stringhunt.entities.panels;
+
+import java.awt.Graphics;
+
+import com.stringhunt.Handler;
+import com.stringhunt.entities.Entity;
+
+public class PausePanel extends Entity {
+    
+    private Handler handler;
+
+    
+    public PausePanel(float x, float y, int width, int height) {
+	super(x, y, width, height);
+	this.handler = handler;
+    }
+
+    @Override
+    public void tick() {
+
+	
+    }
+
+    @Override
+    public void render(Graphics g) {
+	
+    }
+    
+   
+}
