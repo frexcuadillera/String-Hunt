a109a560d2fb53d1a22b8ab72807a204e5550f80
frexcuadillera <fcuadillera@gmail.com>
2019-04-06 23:20:28 +0800

    rename Launcher -> StringHuntGameLauncher

--- src/com/stringhunt/StringHuntGameLauncher.java
+++ src/com/stringhunt/StringHuntGameLauncher.java
@@ -1,6 +1,6 @@
 package com.stringhunt;
 
-public class Launcher {
+public class StringHuntGameLauncher {
     
     public static void main(final String [] args) {
 	Game game = new Game("String Hunt", 800, 450);
