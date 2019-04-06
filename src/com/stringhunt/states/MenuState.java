ed2deda179155b2685d06a0e95d5d94893388587
frexcuadillera <fcuadillera@gmail.com>
2019-04-06 21:55:41 +0800

    use handler

--- src/com/stringhunt/states/MenuState.java
+++ src/com/stringhunt/states/MenuState.java
@@ -1,25 +1,33 @@
 package com.stringhunt.states;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
-import com.stringhunt.Game;
+import com.stringhunt.Handler;
 
 public class MenuState extends State {
     
-    public MenuState(Game game) {
-	super(game);
+    public MenuState(Handler handler) {
+	super(handler);
 	
     }
 
     @Override
     public void tick() {
-	// TODO Auto-generated method stub
+	//System.out.println("x: " +handler.getMouseManager().getMouseX()+ " y: " +handler.getMouseManager().getMouseY());
+//	if(handler.getMouseManager().isLeftPressed()) {
+//	    State.setState(handler.getGame().gameState);
+//	}
+	
+	
 	
     }
 
     @Override
     public void render(Graphics g) {
 	g.drawString("this is menu frame", 60, 60); //temp
+	g.setColor(Color.red);
+	g.fillRect(handler.getMouseManager().getMouseX(), handler.getMouseManager().getMouseY(), 8, 8);
 	
     }
 
