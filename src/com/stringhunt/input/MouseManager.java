01268d101518079f65b6c6839efe5dd5bd5968d4
frexcuadillera <fcuadillera@gmail.com>
2019-04-06 21:54:18 +0800

    Create MouseManager.java

---
+++ src/com/stringhunt/input/MouseManager.java
@@ -0,0 +1,100 @@
+package com.stringhunt.input;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MouseManager implements MouseListener, MouseMotionListener{
+    
+    private boolean leftPressed, rightPressed;
+    private int mouseX, mouseY;
+    
+    
+    public MouseManager() {
+	
+    }
+    
+    // getters
+    
+    public boolean isLeftPressed() {
+        return leftPressed;
+    }
+
+    public boolean isRightPressed() {
+        return rightPressed;
+    }
+
+    public int getMouseX() {
+        return mouseX;
+    }
+
+    public int getMouseY() {
+        return mouseY;
+    }
+
+    // imeplemented methods
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+	
+	if(e.getButton() == MouseEvent.BUTTON1) {
+	    leftPressed = true;
+	}
+	
+	if(e.getButton() == MouseEvent.BUTTON3) {
+	    rightPressed = true;
+	}
+	
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+	
+	if(e.getButton() == MouseEvent.BUTTON1) {
+	    leftPressed = false;
+	}
+	
+	if(e.getButton() == MouseEvent.BUTTON3) {
+	    rightPressed = false;
+	}
+	
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+	
+	mouseX = e.getX();
+	mouseY = e.getY();
+	
+    }
+    
+    @Override
+    public void mouseDragged(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+    }
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+	
+	// pressed and released
+	
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+	
+	// entered a component
+	
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+	
+	// exited a component
+	
+    }
+
+}
