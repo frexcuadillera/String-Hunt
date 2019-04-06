1a1c649d14286c572ade266219a28a51de2fb4ce
frexcuadillera <fcuadillera@gmail.com>
2019-04-07 02:27:36 +0800

    added test enemy sheet

--- src/com/stringhunt/gfx/Assets.java
+++ src/com/stringhunt/gfx/Assets.java
@@ -10,12 +10,22 @@ public class Assets {
     public static BufferedImage[] btn_credits;
     public static BufferedImage[] btn_quit;
     
+    public static SpriteSheet enemy_sheet;
+    
+    public static BufferedImage player;
+    
     public static void init() {
-	//buffered declaration	
+	//buffered image array
 	btn_start = new BufferedImage[2];
 	btn_tutorial = new BufferedImage[2];
 	btn_credits = new BufferedImage[2];
 	btn_quit = new BufferedImage[2];
+	
+	
+	//spritesheets
+	//enemy_sheet = new SpriteSheet(ImageLoader.loadImage("/test/test_sprite.png"));
+	
+	
 	//load image here
 	//testImage = ImageLoader.loadImage("/textures/test.jpg");
 	
@@ -31,9 +41,11 @@ public class Assets {
 	btn_quit[0] = ImageLoader.loadImage("/ui/button_quit_0.png");
 	btn_quit[1] = ImageLoader.loadImage("/ui/button_quit_1.png");
 	
+	//player assets
+	
+	
 	//load spritesheet heres
 	//load image first to convert as spritesheet
-	//sheet = new SpriteSheet(bufferedimagename);
 	
 	//SpriteSheet ui_sheet = new SpriteSheet(ImageLoader.loadImage("/sheets/blueSheet.png"));	
 	//btn_start[0] = ui_sheet.crop(0, 0, 190, 49);
