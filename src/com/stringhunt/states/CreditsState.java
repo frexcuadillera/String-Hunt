ad43681e3c81b67e22ab654857158713662878ff
frexcuadillera <fcuadillera@gmail.com>
2019-04-08 02:23:15 +0800

    added back button

--- src/com/stringhunt/states/CreditsState.java
+++ src/com/stringhunt/states/CreditsState.java
@@ -4,24 +4,41 @@ import java.awt.Graphics;
 
 import com.stringhunt.Handler;
 import com.stringhunt.gfx.Assets;
+import com.stringhunt.ui.UIEvent;
 
 public class CreditsState extends State {
     
-    public CreditsState(Handler handler) {
+    private final int backX = 20;
+    private final int backY = 380;
+    private final int backWidth = 200;
+    private final int backHeight = 50;
+    
+    private int backHover = 0;
+    
+    private UIEvent uiEvent;
+    
+    public CreditsState(final Handler handler) {
 	super(handler);
+	
+	uiEvent = new UIEvent(handler);
 
     }
 
     @Override
     public void tick() {
-
+	backHover = uiEvent.isHovering(backX, backY, backWidth, backHeight);
 	
-    }
-
+	
+	if(backHover == 1 && handler.getMouseManager().isLeftPressed()) {
+	    State.setState(handler.getGame().menuState);
+	}
+	
+    }	
+    
     @Override
     public void render(Graphics g) {
 	g.drawImage(Assets.background, 0, 0, null);
-	
+	g.drawImage(Assets.btn_back[backHover], backX, backY, backWidth, backHeight, null);
     }
 
 }
