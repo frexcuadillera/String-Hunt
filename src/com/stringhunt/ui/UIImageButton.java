5e760cb66c4e1bd7734f2a3c5d30b81a476f1273
frexcuadillera <fcuadillera@gmail.com>
2019-04-07 21:11:21 +0800

    fix hovering render

--- src/com/stringhunt/ui/UIImageButton.java
+++ src/com/stringhunt/ui/UIImageButton.java
@@ -19,10 +19,13 @@ public class UIImageButton extends UIObject {
 
 	@Override
 	public void render(Graphics g) {
-		if(hovering)
-			g.drawImage(images[1], (int) x, (int) y, width, height, null);
-		else
-			g.drawImage(images[0], (int) x, (int) y, width, height, null);
+		if(hovering) {
+		    g.drawImage(images[1], (int) x, (int) y, width, height, null);
+		}			
+		else {
+		    g.drawImage(images[0], (int) x, (int) y, width, height, null);
+		}
+			
 	}
 
 	@Override
