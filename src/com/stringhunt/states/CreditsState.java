585c83cf8ee5aec14f45dc11ed4940ff45ee4db7
frexcuadillera <fcuadillera@gmail.com>
2019-04-07 20:44:01 +0800

    added back button

--- src/com/stringhunt/states/CreditsState.java
+++ src/com/stringhunt/states/CreditsState.java
@@ -3,23 +3,39 @@ package com.stringhunt.states;
 import java.awt.Graphics;
 
 import com.stringhunt.Handler;
+import com.stringhunt.gfx.Assets;
+import com.stringhunt.ui.ClickListener;
+import com.stringhunt.ui.UIImageButton;
+import com.stringhunt.ui.UIManager;
 
 public class CreditsState extends State {
 
+    private UIManager uiManager;
+    
     public CreditsState(Handler handler) {
 	super(handler);
-	// TODO Auto-generated constructor stub
+	uiManager = new UIManager(handler);
+	handler.getMouseManager().setUIManager(uiManager);
+	
+	uiManager.addObject(new UIImageButton(50, 50, 200, 50, Assets.btn_start, new ClickListener() {	    
+	    @Override
+	    public void onClick() {
+		State.setState(handler.getGame().menuState);
+	    }
+	    
+	}));
     }
 
     @Override
     public void tick() {
-	// TODO Auto-generated method stub
+	uiManager.tick();
 	
     }
 
     @Override
     public void render(Graphics g) {
-	g.drawString("this is credits frame", 60, 60); //temp
+	g.drawImage(Assets.background, 0, 0, null);
+	uiManager.render(g);
 	
     }
 
