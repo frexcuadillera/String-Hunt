4b3f7726ceafcfb67aef8b87b554d5648451f089
frexcuadillera <fcuadillera@gmail.com>
2019-04-08 02:22:44 +0800

    nothing

--- src/com/stringhunt/StringHuntGameLauncher.java
+++ src/com/stringhunt/StringHuntGameLauncher.java
@@ -1,7 +1,7 @@
 package com.stringhunt;
 
 public class StringHuntGameLauncher {
-    
+
     public static void main(final String [] args) {
 	Game game = new Game("String Hunt", 800, 450);
 	game.start();
