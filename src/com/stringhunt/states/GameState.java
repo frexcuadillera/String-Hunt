43adb35b9bdaf184d66f0612654e399331beb494
frexcuadillera <fcuadillera@gmail.com>
2019-04-09 21:00:37 +0800

    added Panels

--- src/com/stringhunt/states/GameState.java
+++ src/com/stringhunt/states/GameState.java
@@ -1,42 +1,42 @@
 package com.stringhunt.states;
 
-import java.awt.Color;
 import java.awt.Graphics;
 
 import com.stringhunt.Handler;
-import com.stringhunt.entities.creatures.Player;
+import com.stringhunt.entities.panels.*;
+import com.stringhunt.gfx.Assets;
 
 public class GameState extends State {
     
     
     //declare game entities here
-    private Player player;
+    private BoardPanel boardPanel;
+    private LetterPanel letterPanel;
+    private ScenePanel scenePanel;
     
     public GameState(Handler handler) {
 	super(handler);
 	
-	
+	//declare entities x,y,w,h
 	//player = new Player(xpos,ypos, width,height);
-	player = new Player(handler, 100, 100, 50, 50);
-	
+	//player = new Player(handler, 100, 100, 50, 50);
+	boardPanel = new BoardPanel(handler, 20,380,200,50);
+	letterPanel = new LetterPanel(handler, 50,180,700,100);
+	scenePanel = new ScenePanel(handler, 100,100, 100, 100);
     }
 
     @Override
     public void tick() {
-	player.tick();
-	
+	boardPanel.tick();
+	letterPanel.tick();
+	scenePanel.tick();
     }
 
     @Override
     public void render(Graphics g) {
-	g.setColor(Color.black);
-	g.fillRect(0, 0, 800, 225);
-	g.setColor(Color.BLUE);
-	g.fillRect(0, 225, 800, 225);
-	player.render(g);
-	
+	//g.drawImage(Assets.background, 0, 0, null);
+	boardPanel.render(g);
+	letterPanel.render(g);
+	scenePanel.render(g);	
     }
-    
-    
-
 }
