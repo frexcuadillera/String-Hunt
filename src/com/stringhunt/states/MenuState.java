b8ad83435f3dd996690f6cb337fff63051afc955
frexcuadillera <fcuadillera@gmail.com>
2019-04-09 21:00:11 +0800

    added quit JOptionPane

--- src/com/stringhunt/states/MenuState.java
+++ src/com/stringhunt/states/MenuState.java
@@ -2,6 +2,8 @@ package com.stringhunt.states;
 
 import java.awt.Graphics;
 
+import javax.swing.JOptionPane;
+
 import com.stringhunt.Handler;
 import com.stringhunt.gfx.Assets;
 import com.stringhunt.ui.UIEvent;
@@ -13,7 +15,7 @@ public class MenuState extends State {
     private final int width = 200;
     private final int height = 50;
     private final int Y_OFFSET = 60;
-    private int[] hovering = {0,0,0,0};
+    private int[] hovering = {0,0,0,0,0};
     
     private UIEvent uiEvent;
     
@@ -22,15 +24,14 @@ public class MenuState extends State {
 		
 		uiEvent = new UIEvent(handler);
     }
-    
-
 
     @Override
     public void tick() {	
 		hovering[0] = uiEvent.isHovering(X_POSITION, Y_POSITION, width, height);
 		hovering[1] = uiEvent.isHovering(X_POSITION, Y_POSITION + Y_OFFSET, width, height);
-		hovering[2] = uiEvent.isHovering(X_POSITION, Y_POSITION  + Y_OFFSET * 2, width, height);
-		hovering[3] = uiEvent.isHovering(X_POSITION, Y_POSITION  + Y_OFFSET * 3, width, height);
+		hovering[2] = uiEvent.isHovering(X_POSITION, Y_POSITION + Y_OFFSET * 2, width, height);
+		hovering[3] = uiEvent.isHovering(X_POSITION, Y_POSITION + Y_OFFSET * 3, width, height);
+		hovering[4] = uiEvent.isHovering(X_POSITION, Y_POSITION + Y_OFFSET * 4, width, height);
 		
 		if(hovering[0] == 1 && handler.getMouseManager().isLeftPressed()) {
 		    State.setState(handler.getGame().gameState);
@@ -41,11 +42,20 @@ public class MenuState extends State {
 		}
 		
 		if(hovering[2] == 1 && handler.getMouseManager().isLeftPressed()) {
-		    State.setState(handler.getGame().creditsState);
+		    State.setState(handler.getGame().descriptionState);
 		}
 		
 		if(hovering[3] == 1 && handler.getMouseManager().isLeftPressed()) {
-		    System.out.println("quit pressed");
+		    State.setState(handler.getGame().creditsState);
+		}
+		
+		if(hovering[4] == 1 && handler.getMouseManager().isLeftPressed()) {
+	 	    
+		    int quitOption = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?","String Hunt",JOptionPane.YES_NO_OPTION);
+		    
+		    if(quitOption == 0) {
+			System.exit(0);
+		    }		    
 		}
     }
 
@@ -54,8 +64,9 @@ public class MenuState extends State {
 		g.drawImage(Assets.background, 0, 0, null);
 		g.drawImage(Assets.btn_start[hovering[0]], X_POSITION, Y_POSITION, width, height, null);
 		g.drawImage(Assets.btn_tutorial[hovering[1]], X_POSITION, Y_POSITION + Y_OFFSET, width, height, null);
-		g.drawImage(Assets.btn_credits[hovering[2]], X_POSITION, Y_POSITION + Y_OFFSET * 2, width, height, null);
-		g.drawImage(Assets.btn_quit[hovering[3]], X_POSITION, Y_POSITION + Y_OFFSET * 3, width, height, null);
+		g.drawImage(Assets.btn_description[hovering[2]], X_POSITION, Y_POSITION + Y_OFFSET * 2, width, height, null);
+		g.drawImage(Assets.btn_credits[hovering[3]], X_POSITION, Y_POSITION + Y_OFFSET * 3, width, height, null);
+		g.drawImage(Assets.btn_quit[hovering[4]], X_POSITION, Y_POSITION + Y_OFFSET * 4, width, height, null);
     }
 
 }
