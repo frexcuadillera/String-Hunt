877647b46d6f1f7c46c8df4957e224fc951f8b5b
frexcuadillera <fcuadillera@gmail.com>
2019-04-09 21:04:07 +0800

    added handler, float x, y -> int x, y

--- src/com/stringhunt/entities/panels/ScenePanel.java
+++ src/com/stringhunt/entities/panels/ScenePanel.java
@@ -2,24 +2,24 @@ package com.stringhunt.entities.panels;
 
 import java.awt.Graphics;
 
+import com.stringhunt.Handler;
 import com.stringhunt.entities.Entity;
 
 public class ScenePanel extends Entity {
-
-    public ScenePanel(float x, float y, int width, int height) {
-	super(x, y, width, height);
-	// TODO Auto-generated constructor stub
+    
+    public ScenePanel(Handler handler, int x, int y, int width, int height) {
+	super(handler, x, y, width, height);
     }
 
     @Override
     public void tick() {
-	// TODO Auto-generated method stub
+	
 	
     }
 
     @Override
     public void render(Graphics g) {
-	// TODO Auto-generated method stub
+
 	
     }
 
