3485aa490dfa998de11be0514056a9e2465308cb
frexcuadillera <fcuadillera@gmail.com>
2019-04-09 20:59:55 +0800

    added back button

--- src/com/stringhunt/states/TutorialState.java
+++ src/com/stringhunt/states/TutorialState.java
@@ -3,23 +3,41 @@ package com.stringhunt.states;
 import java.awt.Graphics;
 
 import com.stringhunt.Handler;
+import com.stringhunt.gfx.Assets;
+import com.stringhunt.ui.UIEvent;
 
 public class TutorialState extends State {
 
+    private final int backX = 20;
+    private final int backY = 380;
+    private final int backWidth = 200;
+    private final int backHeight = 50;
+    
+    private int backHover = 0;
+    
+    private UIEvent uiEvent;
+    
     public TutorialState(Handler handler) {
 	super(handler);
-	// TODO Auto-generated constructor stub
+	uiEvent = new UIEvent(handler);
     }
 
     @Override
     public void tick() {
-	// TODO Auto-generated method stub
+	
+	backHover = uiEvent.isHovering(backX, backY, backWidth, backHeight);
+	
+	
+	if(backHover == 1 && handler.getMouseManager().isLeftPressed()) {
+	    State.setState(handler.getGame().menuState);
+	}
 	
     }
 
     @Override
     public void render(Graphics g) {
-	g.drawString("this is tutorial frame", 60, 60);
+	g.drawImage(Assets.background, 0, 0, null);
+	g.drawImage(Assets.btn_back[backHover], backX, backY, backWidth, backHeight, null);
 	
     }
 
