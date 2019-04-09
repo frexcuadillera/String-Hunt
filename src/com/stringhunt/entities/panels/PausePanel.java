4522952f2176fb697e2fcd2d55a861afca3b3157
frexcuadillera <fcuadillera@gmail.com>
2019-04-09 21:03:33 +0800

    float x, y -> int x, y

--- src/com/stringhunt/entities/panels/PausePanel.java
+++ src/com/stringhunt/entities/panels/PausePanel.java
@@ -8,12 +8,9 @@ import com.stringhunt.Handler;
 import com.stringhunt.entities.Entity;
 
 public class PausePanel extends Entity {
-    
-    private Handler handler;
-
-    
-    public PausePanel(float x, float y, int width, int height) {
-	super(x, y, width, height);
+   
+    public PausePanel(Handler handler, int x, int y, int width, int height) {
+	super(handler, x, y, width, height);
     }
 
     @Override
