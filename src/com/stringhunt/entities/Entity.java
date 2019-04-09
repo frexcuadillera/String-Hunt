69264e1f2832e7ce93341ec55e12c725f422d2b2
frexcuadillera <fcuadillera@gmail.com>
2019-04-09 21:01:39 +0800

    added Handler to constructor, changed x, y datatype to integer

--- src/com/stringhunt/entities/Entity.java
+++ src/com/stringhunt/entities/Entity.java
@@ -2,31 +2,35 @@ package com.stringhunt.entities;
 
 import java.awt.Graphics;
 
+import com.stringhunt.Handler;
+
 public abstract class Entity {
 
-    protected float x, y;
+    protected Handler handler;
+    protected int x, y;
     protected int width, height;	
     
-    public Entity(float x, float y, int width, int height) {
+    public Entity(Handler handler, int x, int y, int width, int height) {
+	this.handler = handler;
 	this.x = x;
 	this.y = y;
 	this.width = width;
 	this.height = height;
     }
     
-    public float getX() {
+    public int getX() {
         return x;
     }
 
-    public void setX(float x) {
+    public void setX(int x) {
         this.x = x;
     }
 
-    public float getY() {
+    public int getY() {
         return y;
     }
 
-    public void setY(float y) {
+    public void setY(int y) {
         this.y = y;
     }
 
